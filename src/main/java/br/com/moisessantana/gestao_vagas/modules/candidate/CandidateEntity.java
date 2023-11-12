d4e43a897763e40377bb43bbb299f553fe62e8f0
Moisés Santana <moisaant@gmail.com>
2023-11-12 17:47:02 -0300

    feat: persist candidate

--- src/main/java/br/com/moisessantana/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/moisessantana/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,16 +1,27 @@
 package br.com.moisessantana.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+  
   private String name;
 
   @Pattern(regexp = "\\S+", message = "O campo (username) não pode estar vazio nem conter espaços.")
@@ -21,6 +32,11 @@ public class CandidateEntity {
 
   @Length(min = 6, message = "O campo (password) deve conter no mínimo 6 caracteres.")
   private String password;
+  
   private String description;
+  
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
